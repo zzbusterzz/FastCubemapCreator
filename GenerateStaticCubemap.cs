aecda2f1d9a6819aae40229d674940274c5cb094
Mangirish Naik <naikmangirish@gmail.com>
2016-12-06 15:50:01 +0800

    Added Cubemap Generator script for the scene
    First Commit

---
+++ GenerateStaticCubemap.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class GenerateStaticCubemap : ScriptableWizard {
+
+	public Transform renderPosition;
+	public Cubemap cubemap;
+
+	void OnWizardUpdate() {
+		helpString = "Select transform to render" +
+			" from and cubemap to render into";
+		if (renderPosition != null && cubemap != null) {
+			isValid = true;
+		}
+		else {
+			isValid = false;
+		}
+	}
+
+	void OnWizardCreate() {
+		GameObject go = new GameObject("CubeCam", typeof(Camera));
+
+		go.transform.position = renderPosition.position;
+		go.transform.rotation = Quaternion.identity;
+
+		go.GetComponent<Camera>().RenderToCubemap(cubemap);
+
+		DestroyImmediate(go);
+	}
+
+	[MenuItem("CookBook/Render Cubemap")]
+	static void RenderCubemap() {
+		ScriptableWizard.DisplayWizard("Render CubeMap", typeof(GenerateStaticCubemap), "Render!");
+	}
+}
